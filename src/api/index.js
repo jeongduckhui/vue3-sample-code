6f352a77a7f17595ee2504209a6b62b971619348
duckis <jeongduckhui@gmail.com>
2025-10-27 21:53:54 +0900

    axiosRetry 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,32 @@
 import axios from 'axios'
+import axiosRetry from 'axios-retry'
 
-const baseURL = import.meta.env.VITE_APP_API_URL
+const API_BASE_URL = import.meta.env.VITE_APP_API_URL
 
-function create(baseURL, options) {
-  const instance = axios.create(Object.assign({ baseURL }, options))
+function createAxiosInstance(baseURL, options) {
+  const instance = axios.create(
+    Object.assign(
+      {
+        baseURL,
+        timeout: 10000, // 10초 타임아웃. 일단 10초로 하고 상황에 맞게 수정해야 함
+      },
+      options,
+    ),
+  )
+
+  axiosRetry(instance, {
+    retries: 3, // 재시도 횟수 설정
+    retryDelay: axiosRetry.exponentialDelay, // 지수 백오프 방식의 지연 시간 설정 (권장)
+    retryCondition: error => {
+      // 요청 메서드가 'get'인 경우에만 재시도
+      return (
+        error.config.method === 'get' &&
+        axiosRetry.isNetworkOrIdempotentRequestError(error)
+      )
+    },
+  })
 
   return instance
 }
 
-export const axiosInstance = create(baseURL)
+export const axiosInstance = createAxiosInstance(API_BASE_URL)
