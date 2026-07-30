f352110e2dde1e6a69c218fb97a24e2e731df86c
jeongduckhui <40081877+jeongduckhui@users.noreply.github.com>
2026-07-30 11:27:13 +0900

    Update memo.jsx

--- src/memo/memo.jsx
+++ src/memo/memo.jsx
@@ -1,25 +1,32 @@
-const GridWrapper = styled.div`
-  .ag-header-cell[col-id="NET_DIE"] {
-    position: relative;
+function addLeftBorderToFirstPath(column) {
+  // 부모 그룹 컬럼
+  if (column.children?.length) {
+    const [firstChild, ...otherChildren] = column.children;
+
+    return {
+      ...column,
+      headerGroupClass: [
+        column.headerGroupClass,
+        "dynamic-first-left-border",
+      ]
+        .filter(Boolean)
+        .join(" "),
+
+      children: [
+        addLeftBorderToFirstPath(firstChild),
+        ...otherChildren,
+      ],
+    };
   }
 
-  .ag-header-cell[col-id="NET_DIE"]::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 1px;
-    height: 100%;
-
-    background: repeating-linear-gradient(
-      to bottom,
-      #bdc3c7 0,
-      #bdc3c7 31px,
-      transparent 31px,
-      transparent 32px
-    );
-
-    z-index: 10;
-    pointer-events: none;
-  }
-`;
+  // 마지막 leaf 컬럼
+  return {
+    ...column,
+    headerClass: [
+      column.headerClass,
+      "dynamic-first-left-border",
+    ]
+      .filter(Boolean)
+      .join(" "),
+  };
+}
